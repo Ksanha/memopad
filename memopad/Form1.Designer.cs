39529ad799252ece1277c59c7d59105bb9d98798
Ksanha <103122151+Ksanha@users.noreply.github.com>
2022-10-10 05:49:33 +0900

    update body
    added

---
+++ memopad/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿
+namespace memopad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.메뉴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.내보내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.인터페이스ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.백그라운드컬러ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.폰트설정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(13, 177);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 289);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(538, 177);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(250, 289);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "완료>>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(333, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<<<미완료";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(537, 25);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(608, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 24);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "일정 저장";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "일정을 입력하세요";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "해야할 일정";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(565, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "완료한 일정";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(333, 397);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 45);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "일정 삭제";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.메뉴ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 메뉴ToolStripMenuItem
+            // 
+            this.메뉴ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.내보내기ToolStripMenuItem,
+            this.정보ToolStripMenuItem,
+            this.인터페이스ToolStripMenuItem});
+            this.메뉴ToolStripMenuItem.Name = "메뉴ToolStripMenuItem";
+            this.메뉴ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.메뉴ToolStripMenuItem.Text = "메뉴";
+            // 
+            // 내보내기ToolStripMenuItem
+            // 
+            this.내보내기ToolStripMenuItem.Name = "내보내기ToolStripMenuItem";
+            this.내보내기ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.내보내기ToolStripMenuItem.Text = "내보내기";
+            this.내보내기ToolStripMenuItem.Click += new System.EventHandler(this.내보내기ToolStripMenuItem_Click);
+            // 
+            // 정보ToolStripMenuItem
+            // 
+            this.정보ToolStripMenuItem.Name = "정보ToolStripMenuItem";
+            this.정보ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.정보ToolStripMenuItem.Text = "정보";
+            this.정보ToolStripMenuItem.Click += new System.EventHandler(this.정보ToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "텍스트 파일 (*.txt) |*.txt|모든 파일(*.*)|*.*";
+            // 
+            // 인터페이스ToolStripMenuItem
+            // 
+            this.인터페이스ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.백그라운드컬러ToolStripMenuItem,
+            this.폰트설정ToolStripMenuItem});
+            this.인터페이스ToolStripMenuItem.Name = "인터페이스ToolStripMenuItem";
+            this.인터페이스ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.인터페이스ToolStripMenuItem.Text = "인터페이스";
+            this.인터페이스ToolStripMenuItem.Click += new System.EventHandler(this.인터페이스ToolStripMenuItem_Click);
+            // 
+            // 백그라운드컬러ToolStripMenuItem
+            // 
+            this.백그라운드컬러ToolStripMenuItem.Name = "백그라운드컬러ToolStripMenuItem";
+            this.백그라운드컬러ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.백그라운드컬러ToolStripMenuItem.Text = "백그라운드 컬러";
+            this.백그라운드컬러ToolStripMenuItem.Click += new System.EventHandler(this.백그라운드컬러ToolStripMenuItem_Click);
+            // 
+            // 폰트설정ToolStripMenuItem
+            // 
+            this.폰트설정ToolStripMenuItem.Name = "폰트설정ToolStripMenuItem";
+            this.폰트설정ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.폰트설정ToolStripMenuItem.Text = "폰트 설정";
+            this.폰트설정ToolStripMenuItem.Click += new System.EventHandler(this.폰트설정ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "memopad";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 내보내기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 정보ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 인터페이스ToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem 백그라운드컬러ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 폰트설정ToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ memopad/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace memopad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//일정저장 버튼
+        {
+            if (textBox1.Text != "") {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//완료 버튼
+        {
+            if (listBox1.Text != null) {
+                listBox2.Items.Add(listBox1.Text);
+                listBox1.Items.Remove(listBox1.Text);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)//미완료 버튼
+        {
+            if (listBox2.Text != null)
+            {
+                listBox1.Items.Add(listBox2.Text);
+                listBox2.Items.Remove(listBox2.Text);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //일정 삭제
+        {
+            if (listBox1.SelectedIndex > -1)
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(" 만든이: 김산하  2102110700  AI융합소프트웨어학과\nC# 기말고사 과제", "프로그램 정보",
+                MessageBoxButtons.OK,(MessageBoxIcon.Information));
+        }
+
+        private void 내보내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            {
+                StreamWriter sw;
+                sw = new StreamWriter("memopad.txt");
+                int count = listBox1.Items.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    listBox1.Items[i] += "\n"; sw.Write(listBox1.Items[i]);
+                }
+                sw.Close();             
+                Process.Start("memopad.txt");
+            }
+        }
+
+        private void 인터페이스ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void 백그라운드컬러ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            this.BackColor = colorDialog1.Color;
+        }
+
+        private void 폰트설정ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            textBox1.Font = fontDialog1.Font;
+            button1.Font = fontDialog1.Font;
+            button2.Font = fontDialog1.Font;
+            button3.Font = fontDialog1.Font;
+            button4.Font = fontDialog1.Font;
+            listBox1.Font = fontDialog1.Font;
+            listBox2.Font = fontDialog1.Font;
+            label1.Font = fontDialog1.Font;
+            label2.Font = fontDialog1.Font;
+            label3.Font = fontDialog1.Font;
+            menuStrip1.Font = fontDialog1.Font;
+        }
+
+        /*private void 불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Filter = "텍스트 파일 (*.txt) |*.txt|모든 파일(*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+
+            listBox1.Items.Add(textBox1.Text);
+            string route = openFileDialog1.InitialDirectory;
+
+            string[] lines = File.ReadAllLines(route);
+            foreach (string show in lines)
+            {
+                Console.WriteLine("{0}", show);
+                listBox1.Items.Add(File.ReadAllLines("*.txt"));
+                textBox1.Text = "";
+            }*/
+    }
+}
+
+
+
